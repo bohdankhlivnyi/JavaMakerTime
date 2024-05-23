c35c3384287f454fb0ca2fcc4222b945e427200f
bohdankhlivnyi <108021616+bohdankhlivnyi@users.noreply.github.com>
2024-05-23 21:07:05 +0300

    Delete MakerTimeJava directory

--- MakerTimeJava/src/main/java/org/example/MonsterNames.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class MonsterNames {
-    public static final String[] NAMES = {
-            "Bandit", "Orc", "Troll", "Vampire", "Werewolf", "Dragon", "Zombie", "Skeleton", "Giant", "Demon", "Wendigo"
-    };
-}
